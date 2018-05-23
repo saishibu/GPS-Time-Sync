fbcfdfd87762f9b0c09f4ebd5ff3411875259859
saishibu38380 <saishibu38380@gmail.com>
2018-05-24 01:37:34 +0530

    v

--- test1.py
+++ test1.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
 
 import os,pynmea2,serial
-
+import datetime
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600)
 a=1
 while a:
 	rcv = port.readline()
 	print rcv[0:6]
-	if rcv[0:6] == '$GPGGA':
+	if rcv[0:6] == '$GPRMC':
 		msg=pynmea2.parse(rcv)
 		print msg
-		time=msg.timestamp
+		time=msg.datetime
 		print time
-		#os.system('date -s "2 OCT 2006 18:00:00"')
+		time=time.replace(microsecond=0)
+		time=time+datetime.timedelta(hours=5.3)
+		time=time.strftime("%d/%b/%Y  %H:%M:%S")
+		#time=datetime.datetime(time)
+		time=unicode(time)
+		print time
+		dateset= 'date -s +'+time
+		print dateset
+		os.system(dateset)
 		#os.system('date')
 		print "GPS Time Syncronized"
 		a=0
