a5c367539fdad3b48f7ee9d45d2bebbe65270d36
Sai Shibu <saishibu38380@gmail.com>
2018-06-20 14:08:30 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 
-import sys,os
-import datetime
+import os,pynmea2
 
-os.system('date -s "2 OCT 2006 18:00:00"')
-os.system('date')
+
+
+a=1
+while a:
+	rcv = port.readline()
+	print rcv[0:6]
+	if rcv[0:6] == '$GPGGA':
+		msg=pynmea2.parse(rcv)
+		time=msg.timestamp
+		print lat
+    #os.system('date -s "2 OCT 2006 18:00:00"')
+    #os.system('date')
+		print "GPS Time Syncronized"
+		a=0
