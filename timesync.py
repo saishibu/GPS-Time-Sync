461e6e8b947dd61f6cb4b7a2d86e1d498848dcd8
Sai Shibu <saishibu38380@gmail.com>
2018-06-20 17:02:12 +0530

    Final time sync

--- timesync.py
+++ timesync.py
@@ -11,19 +11,16 @@ while a:
 	print rcv[0:6]
 	if rcv[0:6] == '$GPRMC':
 		msg=pynmea2.parse(rcv)
-		print msg
+		
 		time=msg.datetime
-		print time
+		
 		time=time.replace(microsecond=0)
 		time=time+datetime.timedelta(hours=5.3)
-		#time=time.strftime("%d/%b/%Y  %H:%M:%S")
-		#time=datetime.datetime(time)
-		#time=unicode(time)
+		
 		print time
-		#dateset= 'date -s +'+time
-		#print dateset
+		
 		subprocess.call(['sudo', 'date', '-s', '{:}'.format(time)]) #Sets system time (Requires root, obviously)
-		#os.system(dateset)
+		
 		os.system('date')
 		print "GPS Time Syncronized"
 		a=0
